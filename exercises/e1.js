d75e57768dac1f963cda23eedb82ff01e543ca19
Rogelio Servin <manchaster1509@gmail.com>
2023-03-08 22:15:54 -0600

    completed e1

--- exercises/e1.js
+++ exercises/e1.js
@@ -5,7 +5,11 @@
 
 export function getFirstTenNumbers() {
   // Your code goes here...
-
+  var numbers = []
+  for (let i = 1; i < 11; i++) {
+    numbers.push(i)
+  }
+  return numbers
 }
  
 // === TEST YOURSELF ===
