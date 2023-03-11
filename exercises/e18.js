6ecae99a60afce3c5af5fa5aa743834ebf526fe7
Rogelio Servin <manchaster1509@gmail.com>
2023-03-10 20:28:48 -0600

    c. e18

--- exercises/e18.js
+++ exercises/e18.js
@@ -4,7 +4,16 @@
 // joinToString(['a', 'b', 'c'], '-') => 'a-b-c'
 // NOTE: You can NOT use the array.join(), array.toString(), and array.replace() methods in your code
 
-export function joinToString(array, separator) {}
+export function joinToString(array, separator) {
+    var joined = ""
+    for (let index = 0; index < array.length; index++) {
+        const elem = array[index];
+        joined += elem
+        if (index != array.length -1) {
+            joined += separator
+        }
+  } return joined
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-18"
